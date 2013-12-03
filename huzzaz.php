562cadf8250b1c7a9c3d72fbbcb61fc67072cd4e
deshine <deshine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-03 21:45:52 +0000

    version 2.2
    git-svn-id: https://plugins.svn.wordpress.org/huzzaz-video-gallery/tags/2.2@814644 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- huzzaz.php
+++ huzzaz.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Huzzaz Video Gallery
 Plugin URI: http://about.huzzaz.com/videogallery
-Description: An awesome, easy to use YouTube and Vimeo video gallery powered by Huzzaz. Activate and use the shortcode: [huzzaz id="?" vpp="?" height="?"] Visit the plugin site for more details.
-Version: 1.0
+Description: An awesome, easy to use YouTube and Vimeo video gallery powered by Huzzaz. Activate and use the shortcode: [huzzaz id="?" vpp="?" height="?" bg="?" color="?" button="?" highlight="?"]. Register at huzzaz.com/beta/join?src=wp to create a video collection. Visit the plugin site for more details.
+Version: 2.2
 Author: Huzzaz
 Author URI: http://huzzaz.com
 License: GPL2
@@ -31,9 +31,17 @@ function huzzaz_func( $atts ) {
 		'id' => 'infinitylist',
 		'vpp' => '16',
 		'height' => '1700',
+        'bg' => '',
+        'color' => '',
+        'button' => '',
+        'highlight' => '',
+        'pro' => 0,
 	), $atts ) );
 
-	$gallery = '<div style="width:100%; height: ' . $height . 'px; margin: 0 auto;"><iframe src="http://huzzaz.com/icollection/' . $id . '?vpp=' . $vpp . '" height="100%" width="100%" frameborder="0"></iframe></div>';
+	if( !$pro )
+        $gallery = '<div class="huzzazWrapper" style="width:100%; height: ' . $height . 'px; margin: 0 auto;"><iframe src="http://huzzaz.com/embed/' . $id . '?vpp=' . $vpp . '&bg=' . $bg . '&color=' . $color . '&button=' . $button . '&highlight=' . $highlight .'" height="100%" width="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>';
+    else
+        $gallery = '<div class="huzzazWrapper" style="width:100%; height: ' . $height . 'px; margin: 0 auto;"><iframe src="http://huzzaz.com/proembed/' . $id . '?vpp=' . $vpp . '&bg=' . $bg . '&color=' . $color . '&button=' . $button . '&highlight=' . $highlight .'" height="100%" width="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>';
 
 	return $gallery;
 }
