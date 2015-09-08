d24784c2a201fe334a99e18047d83f75df7ba75e
deshine <deshine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-08 23:39:00 +0000

    tagging versino 6.0
    git-svn-id: https://plugins.svn.wordpress.org/huzzaz-video-gallery/tags/6.0@1241067 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- huzzaz.php
+++ huzzaz.php
@@ -3,7 +3,7 @@
 Plugin Name: Huzzaz Video Gallery
 Plugin URI: http://about.huzzaz.com/videogallery
 Description: An awesome, easy to use YouTube and Vimeo video gallery powered by Huzzaz. Activate and use the shortcode: [huzzaz id="?" vpp="?" bg="?" color="?" button="?" highlight="?"]. Register at huzzaz.com/join?src=wp to create a video collection. Visit the plugin site for more details.
-Version: 5.1
+Version: 6.0
 Author: Huzzaz
 Author URI: http://huzzaz.com
 License: GPL2
@@ -49,12 +49,12 @@ function huzzaz_func( $atts ) {
 
     if( !$pro ) {
         $src = 'https://huzzaz.com/embed/' . $id . '?vpp=' . $vpp . '&bg=' . $bg . '&color=' . $color . '&button=' . $button . '&highlight=' . $highlight;
-        $gallery = '<div class="' . esc_attr($class) . '"><iframe class="hzframe" src="' . esc_url($src) . '" height="100%" width="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen allowTransparency="true"></iframe><script src="https://huzzaz.com/js/hzframe.js"></script></div>';
+        $gallery = '<div class="' . esc_attr($class) . '"><iframe class="hzframe" src="' . esc_url($src) . '" height="1600" width="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen allowTransparency="true"></iframe><script src="https://huzzaz.com/js/hzframe.js"></script></div>';
     } else {
         if ( !$popoutlink ) {
             if ( $layout != "card" ){
                 $src = 'https://huzzaz.com/proembed/' . $id . '?layout=' . $layout . '&vpp=' . $vpp . '&bg=' . $bg . '&color=' . $color . '&button=' . $button . '&highlight=' . $highlight . '&search=' . $search . '&gicon=' . $gicon . '&titleoverlay=' . $titleoverlay . '&showvideos=' . $showvideos . '&arrows=' . $arrows . '&hzauto=' . $autoplay;
-                $gallery = '<div class="' . esc_attr($class) . '"><iframe class="hzframe" src="' . esc_url($src) . '" height="100%" width="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen allowTransparency="true"></iframe><script src="https://huzzaz.com/js/hzframe.js"></script></div>';
+                $gallery = '<div class="' . esc_attr($class) . '"><iframe class="hzframe" src="' . esc_url($src) . '" height="1600" width="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen allowTransparency="true"></iframe><script src="https://huzzaz.com/js/hzframe.js"></script></div>';
             } else {
                 $src = 'https://huzzaz.com/proembed/' . $id . '?layout=' . $layout . '&vpp=' . $vpp . '&bg=' . $bg . '&color=' . $color . '&button=' . $button . '&highlight=' . $highlight . '&search=' . $search . '&gicon=' . $gicon . '&titleoverlay=' . $titleoverlay . '&showvideos=' . $showvideos . '&hzauto=' . $autoplay;
                 $gallery = '<div class="' . esc_attr($class) . '" style="padding-bottom: 54.8%; position: relative; padding-top: 25px; height: 0; margin-bottom: 16px; overflow: hidden;"><iframe style="position: absolute; top: 0; left: 0;" src="' . esc_url($src) . '" height="100%" width="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen allowTransparency="true"></iframe></div>';
